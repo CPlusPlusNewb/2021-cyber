db60f6ef2b1c218bba9c5fea15e7977e920f4fdd
CPlusPlusNewb <54950790+CPlusPlusNewb@users.noreply.github.com>
2020-10-05 09:05:06 -0700

    Create asc2-a.py

---
+++ asc2-a.py
@@ -0,0 +1,16 @@
+asc2-a.py pn
+#variables
+i1= 33
+i2= 48
+i3= 65
+i4= i3+26 #65+26
+i5= 96
+#output
+print(i1, i2, i3, i4, i5)
+c1 = i1
+c2 = i2
+c3 = i3
+c4 = i4
+c5 = i5
+print(" * * * * * * * ")
+print( c1, c2, c3, c4, c5)
