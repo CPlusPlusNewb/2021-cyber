c97a743cb5333544f8642389774fd846e0211221
CPlusPlusNewb <54950790+CPlusPlusNewb@users.noreply.github.com>
2020-10-05 09:08:16 -0700

    Update asc2-a.py

--- asc2-a.py
+++ asc2-a.py
@@ -4,13 +4,13 @@ i1= 33
 i2= 48
 i3= 65
 i4= i3+26 #65+26
-i5= 96
+i5= 97
 #output
 print(i1, i2, i3, i4, i5)
-c1 = i1
-c2 = i2
-c3 = i3
-c4 = i4
-c5 = i5
+c1 = chr(i1)
+c2 = chr(i2)
+c3 = chr(i3)
+c4 = chr(i4)
+c5 = chr(i5)
 print(" * * * * * * * ")
 print( c1, c2, c3, c4, c5)
